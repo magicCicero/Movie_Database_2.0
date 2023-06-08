3f77db21071ebd5dca41b79f3742f533a67ec53d
magicCicero <cicerooantonio20@gmail.com>
2023-06-08 22:37:45 +0200

    added fontawesome and headerstyling

--- src/assets/components/Pagination.jsx
+++ src/assets/components/Pagination.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import "./Pagination.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Pagination({ pageNum, onPageUp, onPageDown }) {
   return (
     <>
-      <button onClick={onPageUp}>Vorwärts</button>
-      <button onClick={onPageDown}>Zurück</button>
-      <h1>Seite:{pageNum}</h1>
+      <div className="pagination-container">
+        <button onClick={onPageDown}>
+          <FontAwesomeIcon icon={faAngleLeft} />{" "}
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </button>
+
+        <h1>Seite:{pageNum}</h1>
+        <button onClick={onPageUp}>
+          <FontAwesomeIcon icon={faAngleRight} />{" "}
+          <FontAwesomeIcon icon={faAngleRight} />
+        </button>
+      </div>
     </>
   );
 }
