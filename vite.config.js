b0d156afdb3ca5be003bc0746b003f3fc1646cc1
magicCicero <cicerooantonio20@gmail.com>
2023-06-18 18:29:48 +0200

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
+export default defineConfig({
+  plugins: [react()],
 });
